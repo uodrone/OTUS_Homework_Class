3dd7226272d5d9293f44448488e143745c6579d7
uodrone <drone_86@mail.ru>
2024-12-29 09:42:05 +0400

    Анонимные планеты

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using OTUS_Homework_Class.Interfaces;
-using System.ComponentModel;
+﻿using System.Numerics;
 
 namespace OTUS_Homework_Class
 {
@@ -8,23 +7,66 @@ namespace OTUS_Homework_Class
 
         static void Main()
         {
-            var quadcopter = new Quadcopter();            
+            PlanetCreate();
+        }
 
-            Console.WriteLine($"Информация о зарядке: {quadcopter.GetInfo()}, вот посмотри");
-            quadcopter.Charge();
-
-            IRobot robot = new Quadcopter();
-            Console.WriteLine($"Дефолтный метод робота: {robot.GetRobotType()}");
-
-            Console.WriteLine($"Информация о роботе: {robot.GetInfo()}");
-
-            IFlyingRobot flyingRobot = new Quadcopter();
-            Console.WriteLine($"Дефолтный метод летающего робота: {flyingRobot.GetRobotType()}");
-
-            Console.WriteLine("Компоненты робота:");
-            foreach (string component in quadcopter.GetComponents())
+        static void PlanetCreate ()
+        {
+            var Venus = new
             {
-                Console.WriteLine(component);
+                Name = "Венера",
+                Number = 2,
+                EquatorLength = 38025,
+                PrevPlanet = "Меркурий" //если с new Object() то equals не сработает со вторым экземпляром
+            };
+
+            var Earth = new
+            {
+                Name = "Земля",
+                Number = 3,
+                EquatorLength = 40075,
+                PrevPlanet = Venus
+            };
+
+            var Mars = new
+            {
+                Name = "Марс",
+                Number = 4,
+                EquatorLength = 21326,
+                PrevPlanet = Earth
+            };
+
+            var OtherVenus = new
+            {
+                Name = "Венера",
+                Number = 2,
+                EquatorLength = 38025,
+                PrevPlanet = "Меркурий"
+            };
+
+            var PlanetList = new List<Object> { Venus, Earth, Mars, OtherVenus };
+
+            foreach (var Planet in PlanetList) {
+                Console.WriteLine("Информация о планете:");
+                Console.WriteLine($"Название: {Planet.GetType().GetProperty("Name").GetValue(Planet)}");
+                Console.WriteLine($"Порядковый номер от Солнца: {Planet.GetType().GetProperty("Number").GetValue(Planet)}");
+                Console.WriteLine($"Длина экватора: {Planet.GetType().GetProperty("EquatorLength").GetValue(Planet)}");
+                Console.WriteLine($"Ссылка на предыдущую планету: {Planet.GetType().GetProperty("PrevPlanet").GetValue(Planet)}");
+
+                if (Planet.Equals(Venus))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Планета эквивалентра Венере");
+                    Console.ResetColor();
+                } 
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Планета НЕ эквивалента Венере");
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine();
             }
         }
     }
