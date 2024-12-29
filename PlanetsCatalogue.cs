6eccaa12606849e705f627fcb40f3729b94dac4a
uodrone <drone_86@mail.ru>
2024-12-29 17:07:30 +0400

    Третья задача

---
+++ PlanetsCatalogue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTUS_Homework_Class
+{
+    public class PlanetsCatalogue
+    {
+        public List<Planet> Planets { get; set; }
+        public int CountRequestPlanet;
+        public delegate string LyambdaValidator(string name);
+
+        public PlanetsCatalogue(List<Planet> planets)
+        {
+            Planets = planets;
+        }
+
+        public ValueTuple<int?, int?, string?> GetPlanet(string name, LyambdaValidator lyambdaValidator)
+        {
+            string error = lyambdaValidator(name);
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                return (null, null, error);
+            }
+            else
+            {
+                foreach (var planet in Planets)
+                {
+                    if (planet.Name == name)
+                    {
+                        return (planet.Number, planet.EquatorLength, null);
+                    }
+                }
+
+                return (null, null, "Не удалось найти планету");
+            }
+        }
+    }
+}
